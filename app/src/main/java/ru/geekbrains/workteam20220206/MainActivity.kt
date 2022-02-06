4722389a8fb4d81860f5668f77221a7e3a1bc3d9
DwarfPorter <mrvlmor@gmail.com>
2022-02-06 21:44:05 +0300

    обработка кнопки 2

--- app/src/main/java/ru/geekbrains/workteam20220206/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/workteam20220206/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.geekbrains.workteam20220206
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.text).text = "Hello PRessed!"
+        }
     }
 }
