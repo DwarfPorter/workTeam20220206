c0063e079d71c6c87fc2484381bc628c798ddc74
Vladimir Morozov <mrvlmor@gmail.com>
2022-02-06 21:52:22 +0300

    обработка кнопки 2 (#3)

--- app/src/main/java/ru/geekbrains/workteam20220206/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/workteam20220206/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.geekbrains.workteam20220206
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
